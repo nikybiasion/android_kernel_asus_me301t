1909ed220485712a25bfd70271fe7dffa5b04d8d
Nikybiasion <root@slackware>
2014-03-13 22:03:39 +0100

    switch do_fsync() to fget_light() Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/sync.c
+++ fs/sync.c
@@ -192,7 +192,7 @@ int vfs_fsync(struct file *file, int datasync)
 {
 	if (!fsync_enabled)
 			return 0;
-			
+
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -201,14 +201,15 @@ static int do_fsync(unsigned int fd, int datasync)
 {
 	struct file *file;
 	int ret = -EBADF;
+	int fput_needed;
 
 	if (!fsync_enabled)
 			return 0;
 
-	file = fget(fd);
+	file = fget_light(fd, &fput_needed);
 	if (file) {
 		ret = vfs_fsync(file, datasync);
-		fput(file);
+		fput_light(file, fput_needed);
 	}
 	return ret;
 }
@@ -217,7 +218,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 	if (!fsync_enabled)
 			return 0;
-			
+
 	return do_fsync(fd, 0);
 }
 
@@ -225,7 +226,7 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
 	if (!fsync_enabled)
 			return 0;
-			
+
 	return do_fsync(fd, 1);
 }
 
@@ -241,7 +242,7 @@ int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
 	if (!fsync_enabled)
 			return 0;
-			
+
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
 		return 0;
 	return vfs_fsync_range(file, pos, pos + count - 1,
